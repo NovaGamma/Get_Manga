e92f6a36c30449d23d4636337edf3e1871c3bae1
NovaGamma <gammanova41@gmail.com>
2020-08-31 12:57:42 +0200

    a

--- Display_Manga.py
+++ Display_Manga.py
@@ -1,9 +1,22 @@
+import os
 import pygame
 
 pygame.init()
 
+def load_chapter(chapter_number):#load in memory a whole chapter
+    for page in os.listdir():
+        if 'Chapter' in page:
+            temp = page.split(" ")
+            number = int(temp[1])
+            if number == chapter_number:
+                chapter.append(pygame.image.load(page).convert())
+
+
 screen=pygame.display.set_mode((1240,980),pygame.RESIZABLE)
 img = pygame.image.load('Chapter 263 page 1.png').convert()
+size = img.get_size()
+print(size)
+input()
 y=10
 while True:
     screen.blit(img,(0,y))
